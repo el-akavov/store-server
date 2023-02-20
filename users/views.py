1fa0ba9a1c5735bc3c368b91b6e7cf40e02fb7a7
el-akavov <akavoveldar3005@gmail.com>
2023-02-20 23:55:55 +0300

    refactore with CBV method login def

--- users/views.py
+++ users/views.py
@@ -1,32 +1,23 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
 from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.messages.views import SuccessMessageMixin
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
 from users.models import User
 from django.urls import reverse, reverse_lazy
 
-def login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user:
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-    else:
-        form = UserLoginForm()
-    context = {'form': form}
-    return render(request, 'users/login.html', context)
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
 
 class UserRegistrationView(CreateView):
     model = User
     form_class = UserRegistrationForm
     template_name = 'users/registration.html'
     success_url = reverse_lazy('users:login')
+    success_message = 'Вы успешно зарегистрировались!'
 
     def get_context_data(self, **kwargs):
         context = super(UserRegistrationView, self).get_context_data()
