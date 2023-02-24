4a7244ae3827f2f96648eb5e8467a56a8e76478c
el-akavov <akavoveldar3005@gmail.com>
2023-02-24 23:25:56 +0300

    refactor login method

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from users.views import login, UserRegistrationView, UserProfileView, logout
+from users.views import LoginView, UserRegistrationView, UserProfileView, logout
 
 app_name = 'users'
 urlpatterns = [
-    path('login/', login, name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     path('registration/', UserRegistrationView.as_view(), name='registration'),
     path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),
     path('logout/', logout, name='logout')
