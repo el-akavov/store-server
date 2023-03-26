fa42b868555c258cf5aa53f96590896e3363adec
el-akavov <akavoveldar3005@gmail.com>
2023-03-27 01:54:50 +0300

    add EmailVerificationView

--- users/views.py
+++ users/views.py
@@ -8,7 +8,7 @@ from django.contrib import auth
 from common.views import TitleMixin
 from products.models import Basket
 from users.forms import UserLoginForm, UserProfileForm, UserRegistrationForm
-from users.models import User
+from users.models import User, EmailVerification
 
 class UserLoginView(TitleMixin, LoginView):
     template_name = 'users/login.html'
@@ -36,7 +36,22 @@ class UserProfileView(TitleMixin, UpdateView):
         context = super(UserProfileView, self).get_context_data()
         context['baskets'] = Basket.objects.filter(user=self.object)
         return context
-#
+
+class EmailVerificationView(TitleMixin, TemplateView):
+    title = 'Store - Подтверждение электронной почты'
+    template_name = 'users/email_verification.html'
+
+    def get(self, request, *args, **kwargs):
+        code = kwargs['code']
+        user = User.objects.get(email=kwargs['email '])
+        email_verification = EmailVerification.objects.filter(user=user, code=code)
+        if email_verification.exists() and not email_verification.first().is_expired():
+            user.is_verified_email = True
+            user.save()
+            return super(EmailVerificationView, self).get(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('index'))
+
 
 # def registration(request):
 #     if request.method == 'POST':
